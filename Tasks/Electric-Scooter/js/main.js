b22c6cba266a97b38aac3fb4c8963adb1b79dfd5
RajMaraviya <76211782+RajMaraviya@users.noreply.github.com>
2023-04-05 16:15:47 +0530

    5-4-2023

---
+++ Tasks/Electric-Scooter/js/main.js
@@ -0,0 +1,12 @@
+function background1(){
+    document.getElementById("home").style="background-image: url(../images/option-1.jpg);"
+}
+function background2(){
+    document.getElementById("home").style="background-image: url(../images/option-2-0.jpg);"
+}
+function background3(){
+    document.getElementById("home").style="background-image: url(../images/option-4.jpg);"
+}
+function background4(){
+    document.getElementById("home").style="background-image: url(../images/main.jpg);"
+}
